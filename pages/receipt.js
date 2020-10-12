73fe506c21b4dccd32a3e309e4ea0f23f9d6dcd9
Bryan Burke <bryan.t.burke@gmail.com>
2020-10-12 23:25:45 +0300

    creates a receipt review page with editing

--- pages/receipt.js
+++ pages/receipt.js
@@ -1,55 +1,87 @@
-import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Nav from '../components/nav';
+import styles from '../styles/receipt.module.scss';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { forEachObjIndexed, set } from 'ramda';
 
 export default function ReceiptPage() {
+    const [editing, setEditing] = useState(null);
+    const [ref, setRef] = useState(null);
+    useEffect(() => {
+        if (ref !== null) {
+            ref.focus();
+        }
+        setRef(null);
+    });
+
+    const rcpt = {
+        vendor: 'Bauhof Group AS',
+        date: '09/27/2020',
+        id: '1010500205581',
+        tax_id: 'EE100589638',
+        total: '94.74E',
+        vat: '15.79E',
+    }
     return (
         <div className="">
-            <div className="flex flex-col lg:container lg:mx-auto max-h-screen py-4">
-                <div className="flex-shrink px-8 py-2 mx-auto overflow-y-scroll overflow-x-scroll object-contain max-h-2/3vh">
-                        <TransformWrapper options={{'limitToWrapper': 'true'}}>
+            <div className="flex flex-col lg:container lg:mx-auto max-h-screen">
+                <div className="flex-shrink px-8 py-4 mx-auto overflow-y-scroll overflow-x-scroll object-contain max-h-2/3vh">
+                        <TransformWrapper options={{limitToWrapper: true}} reset={{disabled: true}}>
                             <TransformComponent>
                                 <img className="h-full object-fill" src="/test.jpg" alt="test" />
                             </TransformComponent>
                         </TransformWrapper>
                 </div>
                 <div className="max-h-1/3vh px-8 py-4 h-1/4vh"> 
-                    <div className="flex flex-col flex-wrap">
-                            <div className="text-xs text-accent-1 font-bold">
-                                VENDOR
-                            </div>
-                            <div className="text-white">
-                                Bauhof Group AS
-                            </div>
-                            <div className="text-xs text-accent-1 font-bold">
-                                VENDOR
-                            </div>
-                            <div className="text-white">
-                                Bauhof Group AS
-                            </div>
-                            <div className="text-xs text-accent-1 font-bold">
-                                VENDOR
-                            </div>
-                            <div className="text-white">
-                                Bauhof Group AS
-                            </div>
-                            <div className="text-xs text-accent-1 font-bold">
-                                VENDOR
-                            </div>
-                            <div className="text-white">
-                                Bauhof Group AS
-                            </div>
-                            <div className="text-xs text-accent-1 font-bold">
-                                VENDOR
-                            </div>
-                            <div className="text-white">
-                                Bauhof Group AS
+                    <div className={styles.container}>
+                            <Field field="vendor" label="VENDOR" value={rcpt.vendor} editing={editing} setEditing={setEditing} setRef={setRef} />
+                            <Field field="date" label="DATE" value={rcpt.date} editing={editing} setEditing={setEditing} setRef={setRef}/>
+                            <Field field="id" label="RECEIPT NUMBER" value={rcpt.id} editing={editing} setEditing={setEditing} setRef={setRef} />
+                            <Field field="tax_id" label="TAX ID" value={rcpt.tax_id} editing={editing} setEditing={setEditing} setRef={setRef} />
+                            <Field field="total" label="TOTAL" value={rcpt.total} editing={editing} setEditing={setEditing} setRef={setRef} />
+                            <Field field="vat" label="VAT" value={rcpt.vat} editing={editing} setEditing={setEditing} setRef={setRef} />
+
+                            <div className={styles.btn}>
+                                <button className="bg-accent-2 w-full text-white px-full py-2 rounded-full font-bold border border-accent-2">
+                                    <span className="px-2"><FontAwesomeIcon icon={faThumbsUp} /></span>  
+                                    <span className="px-2">Everything looks good</span>
+                                </button>
+                                <div className="text-xs text-center text-white">or click on any value to edit</div>
                             </div>
                     </div>
                 </div>
             </div>
         </div>
     );
+}
+
+function Field(props) {
+
+    var localRef = null;
+
+    const { field, label, value, editing, setEditing, setRef } = props;
+    
+    function handleDivClick() {
+        console.log('got click');
+        if (!editing || editing[field] === undefined) {
+         setEditing(Object.assign({}, editing, {[`${field}`]: value}));
+        }
+        setRef(localRef);
+    }
+
+
+    return (
+        <div className={styles[field]} onClick={() => handleDivClick()}>
+            <div className="text-xs text-accent-1 font-bold">{label}</div>
+            <div className="text-white">
+                {editing && editing[field] !== undefined ? <input ref={(input) => { localRef=input; setRef(input);} } className="bg-secondary appearance-none rounded text-white w-full leading-tight" id={field} type="text" value={editing[field]} onChange={() => setEditing(Object.assign({}, editing, {[`${field}`]: localRef.value}))}></input> : value }
+            </div>
+        </div>
+    );
+
+
 }
